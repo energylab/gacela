dd5b82a9e3ac4ee43f5e1a4553bb77bfc608968e
Noah Goodrich <goodrich.noah@gmail.com>
2011-05-16 05:56:21 -0600

    More changes to the samples

--- samples/concrete-inheritance.php
+++ samples/concrete-inheritance.php
@@ -11,22 +11,38 @@ require '_init.php';
 $students = Gacela::instance()->loadMapper('student')->findAll();
 
 ?>
-	<h3>Concrete Table Inheritance</h3>
+<h3>Concrete Table Inheritance</h3>
 
-	<table>
-		<thead>
-			<th>Student</th>
-			<th>House</th>
-		</thead>
-		<tbody>
+<form>
+	<label>Student Name</label>
+	<input type="text" name="fullName" /><br/>
+
+	<select name="houseId">
+	<? foreach($houses as $house): ?>
+
+	<? endforeach; ?>
+	</select>
+</form>
+
+<table>
+	<thead>
+		<th>Student</th>
+		<th>House</th>
+		<th>Options</th>
+	</thead>
+	<tbody>
 <?
-	foreach($students as $student) {
-		echo '<tr>
-				<td>'.$student->fullName.'</td>
-				<td>'.$student->house->houseName.'</td>
-			</tr>';
-	}
+foreach($students as $student) {
+	echo '<tr>
+			<td>'.$student->fullName.'</td>
+			<td>'.$student->house->houseName.'</td>
+			<td>
+				<a href="">Edit</a>
+				<a href="">Delete</a>
+			</td>
+		</tr>';
+}
 ?>
-		</tbody>
-	</table>
+	</tbody>
+</table>
 
